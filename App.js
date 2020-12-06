3c91f57a8945156315c69335ff39b48a9bbf1ca2
Mikhail <mikhail@bambu.life>
2020-12-06 21:00:30 +0300

    added first page

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  ImageBackground,
+} from 'react-native';
+import CarItem from './components/carItem/index';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <CarItem />
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
